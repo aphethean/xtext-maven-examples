bc6276d2d4bf67cd6eae54c61257edb03681d018
Michael Vorburger <mike@vorburger.ch>
2013-02-11 12:46:24 +0100

    The (only once) gen. src/ DOES belong into SCM, of course.. ;-)

---
+++ 2.3.0-example/org.xtext.example.mydsl/src/org/xtext/example/mydsl/validation/MyDslJavaValidator.java
@@ -0,0 +1,13 @@
+package org.xtext.example.mydsl.validation;
+ 
+
+public class MyDslJavaValidator extends AbstractMyDslJavaValidator {
+
+//	@Check
+//	public void checkGreetingStartsWithCapital(Greeting greeting) {
+//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
+//		}
+//	}
+
+}
